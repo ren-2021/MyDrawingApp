281fd761e12273538f5e91aa9eceb0cb30cf3a55
Ren-2021 <ifl.rendelsevilla@gmail.com>
2022-03-02 09:50:26 +0800

    Added MyDrawingApp

---
+++ MyDrawingApp/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyDrawingApp
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp = new Bitmap(1024, 768);
+        Pen pen = new Pen(Color.Black, 5);
+        bool isDrawing = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                isDrawing = false;
+            }
+            else
+            {
+                isDrawing = true;
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                Graphics g = Graphics.FromImage(bmp);
+                g.DrawEllipse(pen, e.X, e.Y, 3, 1);
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            pen.Color = Color.Red;
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            pen.Color = Color.Blue;
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            pen.Color = Color.Green;
+        }
+
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {
+            pen.Color = Color.Purple;
+        }
+
+        private void toolStripButton5_Click(object sender, EventArgs e)
+        {
+            pen.Color = Color.Lime;
+        }
+
+        private void toolStripButton6_Click(object sender, EventArgs e)
+        {
+            if(pictureBox1.Image != null)
+            {
+                Graphics graphic = Graphics.FromImage(pictureBox1.Image);
+                graphic.Clear(Color.Empty);
+                pictureBox1.Image = null;
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "JPEG Image|*.jpg | Bitmap Image|*.bmp| PNG Image|*.png";
+            saveFileDialog.Title = "Save an Image File.";
+            saveFileDialog.ShowDialog();
+            if (saveFileDialog.FileName != "")
+            {
+                FileStream fs = (FileStream)saveFileDialog.OpenFile();
+                switch (saveFileDialog.FilterIndex)
+                {
+                    case 1:
+                        pictureBox1.Image.Save(fs, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        break;
+                    case 2:
+                        pictureBox1.Image.Save(fs, System.Drawing.Imaging.ImageFormat.Bmp);
+                        break;
+                    case 3:
+                        pictureBox1.Image.Save(fs, System.Drawing.Imaging.ImageFormat.Png);
+                        break;
+                }
+            }
+        }
+    }
+}
